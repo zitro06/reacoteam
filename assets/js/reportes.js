84178786e8cba20b475f657c7cadbed04bb6c1ba
eladio rodriguez ortiz <zitro06@gmail.com>
2015-07-09 21:01:04 -0500

    se quitaron los comentarios de la vista

--- assets/js/reportes.js
+++ assets/js/reportes.js
@@ -2,14 +2,14 @@ var app = angular.module('ReportesModule',[])  ;
 app.controller("reportesController",function($scope, $http){
   
     $scope.init = function(){
-    	console.log("inicio peticion de ejemplo");
+    	//console.log("inicio peticion de ejemplo");
 		$http.get("/getDatitos").then(function(result){
-        	console.log("resultado de la peticion");
-        	console.log(result);
+        //	console.log("resultado de la peticion");
+        //	console.log(result);
         	$scope.users= angular.copy(result.data);
         	console.log($scope.user);
 		});
-		console.log("fin peticion de ejemplo");
+		//console.log("fin peticion de ejemplo");
     }
       
     
