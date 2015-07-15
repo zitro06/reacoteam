f21b317a55d6e3b54ad329b89cfcb2b34c54ced7
tlacahuepec <santiesteban@outlook.com>
2015-07-15 11:28:20 -0500

    pruebita :D

--- assets/js/reportes.js
+++ assets/js/reportes.js
@@ -1,15 +1,17 @@
+(function(){
+
 var app = angular.module('ReportesModule',[])  ;
 app.controller("reportesController",function($scope, $http){
   
     $scope.init = function(){
-    	//console.log("inicio peticion de ejemplo");
-		$http.get("/getDatitos").then(function(result){
-        //	console.log("resultado de la peticion");
-        //	console.log(result);
-        	$scope.users= angular.copy(result.data);
-        	console.log($scope.user);
-		});
-		//console.log("fin peticion de ejemplo");
+        //console.log("inicio peticion de ejemplo");
+        $http.get("/getDatitos").then(function(result){
+        //  console.log("resultado de la peticion");
+        //  console.log(result);
+            $scope.users= angular.copy(result.data);
+            console.log($scope.user);
+        });
+        //console.log("fin peticion de ejemplo");
     }
       
     
@@ -17,3 +19,6 @@ app.controller("reportesController",function($scope, $http){
    
 
 });
+
+};)()
+
